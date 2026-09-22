28ce85d69b087afb1b050d3c490ff44c05c50ff8
Rlynei <wirahman15raihan@.com>
2026-09-22 09:25:57 +0700

    Allow both localhost and production for CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://bmkg-web-puce.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000','https://bmkg-web-puce.vercel.app',],
 
     'allowed_origins_patterns' => [],
 
